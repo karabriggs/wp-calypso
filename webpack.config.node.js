c7d127c3e71516a01cda7b8106518de32a16cc44
Jake F - @samouri <samouri@users.noreply.github.com>
2018-02-27 14:57:31 -0500

    Build: Upgrade to Webpack 4
    Update to use webpack 4 and rework some associated parts of the build.
    - CommonsChunkPlugin and the vendor bundle are no more. The splitting work is now handled by webpack itself. See the optimization section of the config.
    - Update the assets writer to output more useful info, including the files in the entry point and in each chunk
    - Update the script that generates stats to be useful for analyze-bundles. The default set of stats output by `-j` were too large for the JSON Serializer
    - The webpack hotloader uses Array.protoyte.includes. This causes a warning to fire once for every module loaded, which was way too noisy. Make the warning only fire once.
    - Isolate tinymce into one chunk using the new cacheGroup setting
    - Add profiling the the build that produces stats in preanalyze-bundles
    
    Co-authored-by: Ben Lowery <blowery@users.noreply.github.com>

--- webpack.config.node.js
+++ webpack.config.node.js
@@ -8,7 +8,6 @@
  * External dependencies
  */
 const fs = require( 'fs' );
-const HappyPack = require( 'happypack' );
 const HardSourceWebpackPlugin = require( 'hard-source-webpack-plugin' );
 const path = require( 'path' );
 const webpack = require( 'webpack' );
@@ -24,6 +23,8 @@ const bundleEnv = config( 'env' );
 /**
  * Internal variables
  */
+const isDevelopment = bundleEnv === 'development';
+
 const commitSha = process.env.hasOwnProperty( 'COMMIT_SHA' ) ? process.env.COMMIT_SHA : '(unknown)';
 
 // disable add-module-exports. TODO: remove add-module-exports from babelrc. requires fixing jest tests
@@ -106,6 +107,7 @@ const webpackConfig = {
 		path: path.join( __dirname, 'build' ),
 		filename: 'bundle.js',
 	},
+	mode: isDevelopment ? 'development' : 'production',
 	module: {
 		rules: [
 			{
@@ -123,7 +125,7 @@ const webpackConfig = {
 			{
 				test: /\.jsx?$/,
 				exclude: /(node_modules|devdocs[\/\\]search-index)/,
-				loader: [ 'happypack/loader' ],
+				use: [ 'thread-loader', babelLoader ],
 			},
 			{
 				test: /node_modules[\/\\](redux-form|react-redux)[\/\\]es/,
@@ -163,7 +165,6 @@ const webpackConfig = {
 			COMMIT_SHA: JSON.stringify( commitSha ),
 			'process.env.NODE_ENV': JSON.stringify( bundleEnv ),
 		} ),
-		new HappyPack( { loaders: [ babelLoader ] } ),
 		new webpack.NormalModuleReplacementPlugin( /^lib[\/\\]abtest$/, 'lodash/noop' ), // Depends on BOM
 		new webpack.NormalModuleReplacementPlugin( /^lib[\/\\]analytics$/, 'lodash/noop' ), // Depends on BOM
 		new webpack.NormalModuleReplacementPlugin( /^lib[\/\\]user$/, 'lodash/noop' ), // Depends on BOM
